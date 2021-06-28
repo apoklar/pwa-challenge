b10b39869dc379ec18e991b226d850375b980ce5
andrp <a.poklar@yahoo.com>
2021-06-27 21:59:45 -0500

    fixed code

--- server.js
+++ server.js
@@ -15,17 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
 // routes
 app.use(require("./routes/api.js"));
-
-// app.listen(PORT, () => {
-//   console.log(`App running on port ${PORT}!`);
-// });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 		useFindAndModify: false,
